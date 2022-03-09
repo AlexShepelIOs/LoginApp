90aa3ef1808e8aae74ac61387a89ba56ef18e401
AlexShepelSwift <76876025+AlexShepelSwift@users.noreply.github.com>
2022-03-09 07:47:45 +0300

    gradientView

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -21,6 +21,17 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let color1 = UIColor(red: 89 / 255, green: 153 / 255, blue: 141 / 255, alpha: 1.0).cgColor
+            let color2 = UIColor(red: 155 / 255, green: 243 / 255, blue: 99 / 255, alpha: 1.0).cgColor
+            let color3 = UIColor(red: 73 / 255, green: 184 / 255, blue: 121 / 255, alpha: 1.0).cgColor
+            
+            let gradientBackground = CAGradientLayer()
+            gradientBackground.frame = self.view.bounds
+            gradientBackground.colors = [color1, color2, color3]
+            gradientBackground.startPoint = CGPoint(x: 0.0, y: 0.0)
+            gradientBackground.endPoint = CGPoint(x: 0.0, y: 1.0)
+            self.view.layer.insertSublayer(gradientBackground, at: 0)
+
     }
     @IBAction func logInButtonTapped() {
         guard userNameTextField.text == "Admin" && passwordTextField.text == "Admin" else {
--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -15,7 +15,19 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        greetingsLabel.text = "Welcome,\(greetings ?? "")!"
+        
+        greetingsLabel.text = "Welcome, \(greetings ?? "")!"
+        
+        let color1 = UIColor(red: 89 / 255, green: 153 / 255, blue: 141 / 255, alpha: 1.0).cgColor
+            let color2 = UIColor(red: 155 / 255, green: 243 / 255, blue: 99 / 255, alpha: 1.0).cgColor
+            let color3 = UIColor(red: 73 / 255, green: 184 / 255, blue: 121 / 255, alpha: 1.0).cgColor
+            
+            let gradientBackground = CAGradientLayer()
+            gradientBackground.frame = self.view.bounds
+            gradientBackground.colors = [color1, color2, color3]
+            gradientBackground.startPoint = CGPoint(x: 0.0, y: 0.0)
+            gradientBackground.endPoint = CGPoint(x: 0.0, y: 1.0)
+            self.view.layer.insertSublayer(gradientBackground, at: 0)
     }
     
     @IBAction func logOutActionButton() {
