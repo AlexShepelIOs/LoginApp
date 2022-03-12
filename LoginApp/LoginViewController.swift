6072418ef87627675307fd93c883695b6d6c198d
AlexShepelSwift <76876025+AlexShepelSwift@users.noreply.github.com>
2022-03-12 18:09:28 +0300

    someBugFixV2

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -12,67 +12,77 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
-    @IBOutlet weak var logInButton: UIButton!
-    @IBOutlet weak var forgotNameButton: UIButton!
-    @IBOutlet weak var forgotPasswordButton: UIButton!
-    
-    
+    private let user = "Admin"
+    private let password = "Admin"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let color1 = UIColor(red: 89 / 255, green: 153 / 255, blue: 141 / 255, alpha: 1.0).cgColor
-            let color2 = UIColor(red: 155 / 255, green: 243 / 255, blue: 99 / 255, alpha: 1.0).cgColor
-            let color3 = UIColor(red: 73 / 255, green: 184 / 255, blue: 121 / 255, alpha: 1.0).cgColor
-            
-            let gradientBackground = CAGradientLayer()
-            gradientBackground.frame = self.view.bounds
-            gradientBackground.colors = [color1, color2, color3]
-            gradientBackground.startPoint = CGPoint(x: 0.0, y: 0.0)
-            gradientBackground.endPoint = CGPoint(x: 0.0, y: 1.0)
-            self.view.layer.insertSublayer(gradientBackground, at: 0)
-
-    }
-    @IBAction func logInButtonTapped() {
-        guard userNameTextField.text == "Admin" && passwordTextField.text == "Admin" else {
-            showAlert(
-                title: "Invalid Login or Password",
-                message: "Please, enter correct login and password"
-            )
-            passwordTextField.text = ""
-            return
-        }
+        let color2 = UIColor(red: 155 / 255, green: 243 / 255, blue: 99 / 255, alpha: 1.0).cgColor
+        let color3 = UIColor(red: 73 / 255, green: 184 / 255, blue: 121 / 255, alpha: 1.0).cgColor
+        
+        let gradientBackground = CAGradientLayer()
+        gradientBackground.frame = self.view.bounds
+        gradientBackground.colors = [color1, color2, color3]
+        gradientBackground.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientBackground.endPoint = CGPoint(x: 0.0, y: 1.0)
+        view.layer.insertSublayer(gradientBackground, at: 0)
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
-        welcomeVC.greetings = userNameTextField.text
+        welcomeVC.user = user
+    }
+    
+    @IBAction func logInButtonTapped() {
+        guard userNameTextField.text == user && passwordTextField.text == password else {
+            showAlert(
+                title: "Invalid Login or Password",
+                message: "Please, enter correct login and password"
+            )
+            return
+        }
+        performSegue(withIdentifier: "openWelcomeVC", sender: nil)
     }
     
     @IBAction func forgotNameButtonTapped() {
-        showAlert(title: "Oops!", message: "Your name is \"Admin\"!")
+        showAlert(title: "Oops!", message: "Your name is \"\(user)\"!")
     }
     
     @IBAction func forgotPasswordButtonTapped() {
-        showAlert(title: "Oops!", message: "Your password is \"Admin\"!")
+        showAlert(title: "Oops!", message: "Your password is \"\(password)\"!")
     }
     @IBAction func unwind(for segue: UIStoryboardSegue) {
-        guard segue.source is WelcomeViewController else { return }
         userNameTextField.text = ""
         passwordTextField.text = ""
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
     }
 }
 
 extension LoginViewController {
     private func showAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.passwordTextField.text = ""
+        }
         alert.addAction(okAction)
         present(alert, animated: true)
     }
 }
 
+extension LoginViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == userNameTextField {
+            passwordTextField.becomeFirstResponder()
+        } else {
+            logInButtonTapped()
+        }
+        return true
+    }
+}
